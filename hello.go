4b1292ca316b5ff84f5acea3d060e8be18105d26
LuYuan <majinhuigo@163.com>
2022-08-10 14:28:51 +0800

    hot-fix

--- hello.go
+++ hello.go
@@ -12,7 +12,7 @@ func main(){
     fmt.Println("hello. Git!")
     fmt.Println("hello. Git!")
     fmt.Println("hello. Git!")
-    fmt.Println("hello. Git!")
+    fmt.Println("hello. hot-fix!")
 
     fmt.Println("first commit!")
     fmt.Println("hot - fix")
