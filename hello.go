647defa88523010c9a51e05502c7359180f2a593
LuYuan <majinhuigo@163.com>
2022-08-10 10:27:58 +0800

    first commit

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    fmt.Println("hello. Git!"): 
+    fmt.Println("hello. Git!")
+    fmt.Println("hello. Git!")
+    fmt.Println("hello. Git!")
+    fmt.Println("hello. Git!")
+    fmt.Println("hello. Git!")
+    fmt.Println("hello. Git!")
+    fmt.Println("hello. Git!")
+    fmt.Println("hello. Git!")
+}
